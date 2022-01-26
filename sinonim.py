5911658523c1b884323f0b18df6d1109de5bad6e
Maxim Meschankin <maxikavm@gmail.com>
2022-01-26 16:08:05 +0500

    26.01.22

--- sinonim.py
+++ sinonim.py
@@ -1,4 +1,17 @@
 n = int(input())
 sdict = dict()
-sdict.
+for i in range(n):
+    k, val = map(str, input().split())
+    sdict[k] = val
+
+to_found = input()
+
+for k, v in sdict.items():
+    if to_found == v:
+        print(k)
+    if to_found == k:
+        print(v)
+
+
+
 
