28b7b3cefc9ca2630fb4cc443b34845824890d9a
Maxim Meschankin <maxikavm@gmail.com>
2022-01-26 15:27:18 +0500

    26.01.22

--- count_word.py
+++ count_word.py
@@ -1,4 +1,4 @@
-inf = open('text1.txt','r', encoding= 'utf')
+inf = open('text1.txt', 'r', encoding='utf')
 words = []
 
 for line in inf.readlines():
@@ -11,13 +11,12 @@ for word in words:
     #     wordsDict[word] = 1
     # else:
     #     wordsDict[word] = wordsDict[word]+1
-    wordsDict[word]=wordsDict.get(word,0) + 1
+    wordsDict[word] = wordsDict.get(word, 0) + 1
 
 # for key, val in wordsDict.items():
 #     print(f'{key} - {val}')
 
-words = [(key,val) for key, val in wordsDict.items()]
-words = sorted(sorted(words, key = lambda x: x[0]), key = lambda  x: x[1], reverse=True)
+words = [(key, val) for key, val in wordsDict.items()]
+words = sorted(sorted(words, key=lambda x: x[0]), key=lambda x: x[1], reverse=True)
 for m in words:
     print(*m)
-
